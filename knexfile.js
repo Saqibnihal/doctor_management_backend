f152714628e4062aa8a6751efcd5228f87c8d0e6
saqibnihal <saqibnihal31@gmail.com>
2025-10-27 20:53:23 +0530

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 const fs = require('fs');
+
+const sharedConfig = {
+  migrations: { directory: './migrations' },
+  seeds: { directory: './seeds' },
+};
 
 module.exports = {
   development: {
-    client: 'mysql2',
+    client: process.env.DB_CLIENT || 'mysql2',
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
@@ -12,13 +17,23 @@ module.exports = {
       database: process.env.DB_NAME,
       ssl: process.env.DB_SSL === 'true'
         ? { ca: fs.readFileSync('./certs/aiven-ca.pem') }
-        : false
+        : false,
     },
-    migrations: {
-      directory: './migrations', 
+    ...sharedConfig,
+  },
+
+  production: {
+    client: process.env.DB_CLIENT || 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: process.env.DB_SSL === 'true'
+        ? { ca: fs.readFileSync('./certs/aiven-ca.pem') }
+        : false,
     },
-    seeds: {
-      directory: './seeds'
-    }
-  }
+    ...sharedConfig,
+  },
 };
