c734e0029d6dda6f8b96373ebaf2aac3c9d13b88
saqibnihal <saqibnihal31@gmail.com>
2025-10-28 12:04:39 +0530

    cors netlify added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 Model.knex(knex);
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-frontend-domain.com'],
+  origin: ['http://localhost:5173', 'https://doctor-managemanet-sys-apk.netlify.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
   credentials: true,
 }));
